4e19c2cc86b395b4c809ca4fb962c0cfdfae3847
dayi <yumibiao10@gmail.com>
2023-04-16 19:58:29 +0800

    new file:   server/file_manager/collect_node.db 	modified:   server/file_manager/lib/dayi-mysql-tables.js 	modified:   server/file_manager/lib/mysql-lib.js 	new file:   server/file_manager/package-lock.json 	modified:   server/file_manager/package.json 	modified:   server/file_manager/yarn-error.log 	modified:   server/file_manager/yarn.lock

--- server/file_manager/lib/dayi-mysql-tables.js
+++ server/file_manager/lib/dayi-mysql-tables.js
@@ -1,35 +1,68 @@
-sql_table_1 = 'CREATE TABLE IF NOT EXISTS `users` \
-( `id` INT  AUTO_INCREMENT , \
-  `user_name` TEXT NOT NULL , \
-  `passwd` TEXT  NULL ,\
-  `permission` INT NOT NULL , \
-  `name` TEXT  NULL ,\
-  `stu_number` INT  NULL ,\
-  `json` JSON  NULL ,\
-  PRIMARY KEY  (`id`)\
-) ENGINE = InnoDB;';
+const sql_table_1 = `CREATE TABLE IF NOT EXISTS users (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT NOT NULL,
+  passwd TEXT NULL,
+  permission INTEGER NOT NULL,
+  name TEXT NULL,
+  stu_number INTEGER NULL,
+  json JSON NULL
+)`;
 
-sql_table_2 = 'CREATE TABLE IF NOT EXISTS  `files` \
-( `id` INT  AUTO_INCREMENT , \
-  `user_name` TEXT NOT NULL ,\
-  `uuid` TEXT NOT NULL ,\
-   `file_path` TEXT  NULL ,\
-    `file_name` TEXT  NULL,\
-    `file_size` DOUBLE NULL, \
-     PRIMARY KEY  (`id`)\
-) ENGINE = InnoDB;';
+const sql_table_2 = `CREATE TABLE IF NOT EXISTS files (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT NOT NULL,
+  uuid TEXT NOT NULL,
+  file_path TEXT NULL,
+  file_name TEXT NULL,
+  file_size REAL NULL
+)`;
+
+const sql_table_3 = `CREATE TABLE IF NOT EXISTS mp3_players (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  own_user_name TEXT NOT NULL,
+  uuid TEXT NOT NULL,
+  is_turn_off_by_admin INTEGER NULL,
+  json JSON NULL
+)`;
+
+exports.sql_table_1 = sql_table_1;
+exports.sql_table_2 = sql_table_2;
+exports.sql_table_3 = sql_table_3;
 
 
-sql_table_3 = 'CREATE TABLE IF NOT EXISTS  `mp3-players` \
-( `id` INT  AUTO_INCREMENT , \
-  `own_user_name` TEXT NOT NULL ,\
-  `uuid` TEXT NOT NULL ,\
-  `is_turn_off_by_admin` INT  NULL ,\
-  `json` JSON  NULL,\
-  PRIMARY KEY  (`id`)\
-) ENGINE = InnoDB;';
+
+// sql_table_1 = 'CREATE TABLE IF NOT EXISTS `users` \
+// ( `id` INT  AUTO_INCREMENT , \
+//   `user_name` TEXT NOT NULL , \
+//   `passwd` TEXT  NULL ,\
+//   `permission` INT NOT NULL , \
+//   `name` TEXT  NULL ,\
+//   `stu_number` INT  NULL ,\
+//   `json` JSON  NULL ,\
+//   PRIMARY KEY  (`id`)\
+// ) ENGINE = InnoDB;';
+
+// sql_table_2 = 'CREATE TABLE IF NOT EXISTS  `files` \
+// ( `id` INT  AUTO_INCREMENT , \
+//   `user_name` TEXT NOT NULL ,\
+//   `uuid` TEXT NOT NULL ,\
+//    `file_path` TEXT  NULL ,\
+//     `file_name` TEXT  NULL,\
+//     `file_size` DOUBLE NULL, \
+//      PRIMARY KEY  (`id`)\
+// ) ENGINE = InnoDB;';
 
 
-exports.sql_table_1=sql_table_1
-exports.sql_table_2=sql_table_2
-exports.sql_table_3=sql_table_3
+// sql_table_3 = 'CREATE TABLE IF NOT EXISTS  `mp3-players` \
+// ( `id` INT  AUTO_INCREMENT , \
+//   `own_user_name` TEXT NOT NULL ,\
+//   `uuid` TEXT NOT NULL ,\
+//   `is_turn_off_by_admin` INT  NULL ,\
+//   `json` JSON  NULL,\
+//   PRIMARY KEY  (`id`)\
+// ) ENGINE = InnoDB;';
+
+
+// exports.sql_table_1=sql_table_1
+// exports.sql_table_2=sql_table_2
+// exports.sql_table_3=sql_table_3
--- server/file_manager/lib/mysql-lib.js
+++ server/file_manager/lib/mysql-lib.js
@@ -1,96 +1,116 @@
-//虽然名字是mysql-lib ,实际上是sqlite3
+//not mysql is sqlite
+//表面上是mysql，实际上是为了兼容原先的代码，文件名和函数名都是mysql，实际上是sqlite
 
 const sqlite = require('better-sqlite3');
-var dayi_mysql_tables = require('./dayi-mysql-tables');
+const dayi_mysql_tables = require('./dayi-mysql-tables');
 
-const db = sqlite('./collect_node.db');
+const db = sqlite('collect_node.db');
 
-
-
-// 创建 users 表
-db.exec(`CREATE TABLE IF NOT EXISTS users (
-  user_name TEXT PRIMARY KEY NOT NULL,
-  permission INTEGER DEFAULT 1
-)`);
-
-// 创建 files 表
-db.exec(`CREATE TABLE IF NOT EXISTS files (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  user_name TEXT NOT NULL,
-  file_path TEXT NOT NULL,
-  uuid TEXT NOT NULL,
-  file_size INTEGER NOT NULL,
-  file_name TEXT NOT NULL,
-  FOREIGN KEY(user_name) REFERENCES users(user_name) ON DELETE CASCADE
-)`);
-
-
-// 创建一个超级用户 admin
-db.get('SELECT * FROM users WHERE user_name = ?', ['admin'], (err, row) => {
-    if (err) {
-        console.error(err.message);
-        throw err;
-    }
-    if (!row) {
-        db.run('INSERT INTO users (user_name, permission) VALUES (?, 9);', ['admin'], (err) => {
-            if (err) {
-                console.error(err.message);
-                throw err;
-            }
-        });
-    }
-});
-
-
-
-// 插入文件记录
-function dayi_insert_file(file_path, user_name, file_uuid, file_size, file_name) {
-  const stmt = db.prepare(`INSERT INTO files (user_name, file_path, uuid, file_size, file_name) VALUES (?, ?, ?, ?, ?)`);
-  stmt.run(user_name, file_path, file_uuid, file_size, file_name);
+// 数据库连接
+try {
+    db.exec('SELECT 1 + 1 AS solution');
+    console.log('[dayi-info] SQLite connect successfully, so 1 + 1 = 2');
+} catch (error) {
+    console.log("[dayi-error]Could not connect to SQLite database.");
+    console.log("[dayi-error]The error is: ", error.message);
+    process.exit(1);
 }
 
-// 插入用户记录
+// 创建表格
+function dayi_create_tables() {
+    try {
+        db.exec(dayi_mysql_tables.sql_table_1);
+        console.log('[dayi-info] Table 1 created successfully');
+        db.exec(dayi_mysql_tables.sql_table_2);
+        console.log('[dayi-info] Table 2 created successfully');
+        db.exec(dayi_mysql_tables.sql_table_3);
+        console.log('[dayi-info] Table 3 created successfully');
+    } catch (error) {
+        console.log('[dayi-error] Error creating tables:', error.message);
+    }
+}
+
+// 插入文件信息
+function dayi_insert_file(file_path, user_name, file_uuid, file_size, file_name) {
+    const stmt = db.prepare('INSERT INTO files (user_name, file_path, uuid, file_size, file_name) VALUES (?, ?, ?, ?, ?)');
+    try {
+        stmt.run(user_name, file_path, file_uuid, file_size, file_name);
+    } catch (error) {
+        console.log('[dayi-error] Error inserting file:', error.message);
+    }
+}
+
+// 插入用户信息
 function dayi_insert_user(user_name) {
-  const stmt = db.prepare(`INSERT INTO users (user_name, permission) VALUES (?, 1)`);
-  stmt.run(user_name);
+    const stmt = db.prepare('INSERT INTO users (user_name, permission) VALUES (?, ?)');
+    try {
+        stmt.run(user_name, 1);
+    } catch (error) {
+        console.log('[dayi-error] Error inserting user:', error.message);
+    }
 }
 
 // 查询用户是否存在于数据库中
 async function dayi_query_user(user_name) {
-  const stmt = db.prepare(`SELECT * FROM users WHERE user_name = ?`);
-  const row = stmt.get(user_name);
-  return row != null;
+    const stmt = db.prepare('SELECT * FROM users WHERE user_name = ?');
+    try {
+        const result = stmt.all(user_name);
+        return result.length >= 1;
+    } catch (error) {
+        console.log('[dayi-error] Error querying user:', error.message);
+        return false;
+    }
 }
 
-// 查询用户的文件记录，如果指定了 file_uuid，则只返回对应文件的记录
+// 查询用户的文件信息
 async function dayi_user_files(user_name, file_uuid) {
-  let sql = `SELECT * FROM files WHERE user_name = ?`;
-  let values = [user_name];
-  if (file_uuid != undefined) {
-    sql += ` AND uuid = ?`;
-    values = [user_name, file_uuid];
-  }
-  const stmt = db.prepare(sql);
-  const rows = stmt.all(values);
-  return rows;
+    let stmt;
+    let sets;
+    if (file_uuid === undefined) {
+        stmt = db.prepare('SELECT * FROM files WHERE user_name = ?');
+        sets = [user_name];
+    } else {
+        stmt = db.prepare('SELECT * FROM files WHERE user_name = ? AND uuid = ?');
+        sets = [user_name, file_uuid];
+    }
+    try {
+        const result = stmt.all(...sets);
+        return result;
+    } catch (error) {
+        console.log('[dayi-error] Error querying user files:', error.message);
+        return [];
+    }
 }
 
-// 删除文件记录
+// 删除文件信息
 async function dayi_delete_file_sql(file_uuid) {
-  const stmt = db.prepare(`DELETE FROM files WHERE uuid = ?`);
-  stmt.run(file_uuid);
+    const stmt = db.prepare('DELETE FROM files WHERE uuid = ?');
+    try {
+        const result = stmt.run(file_uuid);
+        return result;
+    } catch (error) {
+        console.log('[dayi-error] Error deleting file:', error.message);
+        return false;
+    }
 }
 
-exports.dayi_query_user_permisson = (user_name) => {
-  const stmt = db.prepare(`SELECT permission FROM users WHERE user_name = ?`);
-  const row = stmt.get(user_name);
-  return row.permission;
+// 查询用户权限
+async function dayi_query_user_permisson(user_name) {
+    const stmt = db.prepare('SELECT permission FROM users WHERE user_name = ?');
+    try {
+        const result = stmt.get(user_name);
+        return result.permission;
+    } catch (error) {
+        console.log('[dayi-error] Error querying user permission:', error.message);
+        return 0;
+    }
 }
 
+dayi_create_tables();
 
-
-exports.dayi_delete_file_sql = dayi_delete_file_sql
-exports.dayi_user_files = dayi_user_files
-exports.dayi_insert_file = dayi_insert_file
-exports.dayi_insert_user = dayi_insert_user
-exports.dayi_query_user = dayi_query_user
+exports.dayi_query_user_permisson = dayi_query_user_permisson;
+exports.dayi_delete_file_sql = dayi_delete_file_sql;
+exports.dayi_user_files = dayi_user_files;
+exports.dayi_insert_file = dayi_insert_file;
+exports.dayi_insert_user = dayi_insert_user;
+exports.dayi_query_user = dayi_query_user;
