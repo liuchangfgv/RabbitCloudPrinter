cb4fc941659f162434e1957bde6edd63558d1ccc
lc <liuchangfgv@gmail.com>
2023-04-16 10:35:03 +0800

    fix some bug

--- server/together.py
+++ server/together.py
@@ -1,15 +1,14 @@
 from flask import Flask, request, jsonify
 import time
-import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
 clients = {}
 messages = {}
 
-@app.route('/subscribe', methods=['GET'])
-def subscribe():
+@app.route('/subscribe/<group>')
+def subscribe(group):
     global clients
-    group = request.args.get('group')
     if group not in clients:
         clients[str(group)] = 0
         messages[str(group)] = []
@@ -18,16 +17,18 @@ def subscribe():
         while True:
             if len(messages[str(group)]) != 0:
                 clients[str(group)] = clients[str(group)] - 1
-                return jsonify({'message': messages[str(group)][0] })
+                return jsonify({'message': messages[str(group)][0] ,'code':0})
             time.sleep(0.1)
     except Exception as e:
         clients[str(group)] = clients[str(group)] - 1
-        return jsonify({'Error': 'Unknow error' })
+        return jsonify({'Error': 'Unknow error','code':-2})
 
-@app.route('/publish', methods=['POST'])
-def publish():
-    message = request.get_json()['message']
-    group = request.get_json()['group']
+@app.route('/publish/<group>', methods=['POST'])
+def publish(group):
+    try:
+        message = request.get_json()['message']
+    except Exception:
+        return jsonify({'Error': 'No Message','code':-1})
     if group not in clients:
         clients[str(group)] = 0
         messages[str(group)] = []
@@ -35,14 +36,16 @@ def publish():
     while clients[str(group)]:
         pass
     messages[str(group)].remove(message)
-    return jsonify({'message': 'Message sent to all clients'})
+    return jsonify({'message': 'Message sent to all clients','code':0})
 
+
+CORS(app, resources=r'/*') #测试时临时解决跨域问题使用
 if __name__ == '__main__':
     app.run()
 
     
 # 长轮询python实现
 # subscribe curl test：
-#  curl http://localhost:5000/subscribe?group=1
+#  curl http://localhost:5000/subscribe/1
 # publish curl test:
-#  curl -H "Content-Type: application/json" -X POST -d '{"message": "Hello, World!","group":"1"}' http://localhost:5000/publish
+#  curl -H "Content-Type: application/json" -X POST -d '{"message": "Hello, World!"}' http://localhost:5000/publish/1
