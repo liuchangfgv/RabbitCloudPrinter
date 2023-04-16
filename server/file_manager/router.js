0a6ce8c7839e8ac8ad66517ae0bdc3d514cf1b62
dayi <yumibiao10@gmail.com>
2023-04-16 09:49:45 +0800

    ovo

--- server/file_manager/router.js
+++ server/file_manager/router.js
@@ -100,5 +100,29 @@ router.get('/api/delete_file/:uuid', cookieParser(), async function (req, res, n
 });
 
 
+//API-DOWNLOAD-FILE
+router.get('/api/download_file/:uuid', cookieParser(), async function (req, res, next) {
+  const file_uuid = req.params.uuid;
+  const user_cookie = req.cookies[cookie_key];
+  if (!user_cookie) {
+    return res.json({ code: 401, info: '用户未登录' })
+  }
+  const user_name = user_cookie;
+  // 检查用户是否存在
+  const user_exist = await mysql_lib.dayi_query_user(user_name);
+  if (!user_exist) {
+    return res.json({ code: 411, info: '用户不存在，非法操作' })
+  }
+  // 检查用户是否有权限下载该文件
+  const file_info = await mysql_lib.dayi_user_files(user_name, file_uuid);
+  if (!file_info.length) {
+    return res.json({ code: 404, info: '文件不存在或没有权限' })
+  }
+  // 下载文件
+  const file_path = file_info[0].file_path;
+  const file_name = file_info[0].file_name;
+  return res.download(file_path, file_name);
+});
+
 
 module.exports = router;
