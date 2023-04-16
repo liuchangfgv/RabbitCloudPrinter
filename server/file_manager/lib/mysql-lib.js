c451221535ab30953be436d5a3f4108e19d01804
dayi <yumibiao10@gmail.com>
2023-04-16 17:11:14 +0800

    ovo

--- server/file_manager/lib/mysql-lib.js
+++ server/file_manager/lib/mysql-lib.js
@@ -1,144 +1,94 @@
-var mysql = require('mysql')
-var dayi_mysql_tables = require('./dayi-mysql-tables')
+//虽然名字是mysql-lib ,实际上是sqlite3
 
-var connection = mysql.createConnection({
-  host     : '127.0.0.1',
-  user     : 'root',
-  password : '123456',
-  database : 'collect_node'
+const sqlite = require('better-sqlite3');
+var dayi_mysql_tables = require('./dayi-mysql-tables');
+
+const db = sqlite('./collect_node.db');
+
+
+
+// 创建 users 表
+db.exec(`CREATE TABLE IF NOT EXISTS users (
+  user_name TEXT PRIMARY KEY NOT NULL,
+  permission INTEGER DEFAULT 1
+)`);
+
+// 创建 files 表
+db.exec(`CREATE TABLE IF NOT EXISTS files (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  user_name TEXT NOT NULL,
+  file_path TEXT NOT NULL,
+  uuid TEXT NOT NULL,
+  file_size INTEGER NOT NULL,
+  file_name TEXT NOT NULL,
+  FOREIGN KEY(user_name) REFERENCES users(user_name) ON DELETE CASCADE
+)`);
+
+
+// 创建一个超级用户 admin
+db.get('SELECT * FROM users WHERE user_name = ?', ['admin'], (err, row) => {
+    if (err) {
+        console.error(err.message);
+        throw err;
+    }
+    if (!row) {
+        db.run('INSERT INTO users (user_name, permission) VALUES (?, 9);', ['admin'], (err) => {
+            if (err) {
+                console.error(err.message);
+                throw err;
+            }
+        });
+    }
 });
 
 
-//数据库连接
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) {
-    console.log("[dayi-error]Could not connect to MySQL server.");
-    console.log("[dayi-error]The error is: ");  
-    throw error;
-  }
-  console.log('[dayi-info]Mysql connect successfully,so: 1 + 1 =', results[0].solution);
-});
 
-
-//创建表格
-function dayi_create_tables(){
-  connection.query(dayi_mysql_tables.sql_table_1, function (error,results,fields){
-    if(error){console.log(error);return;}
-    console.log('[dayi-info]Table 1 created successfully')
-    // console.log('The results is: ', results);
-  });
-  connection.query(dayi_mysql_tables.sql_table_2,function(error,results,fields){
-    if(error){console.log(error);return;}
-    console.log('[dayi-info]Table 2 created successfully')
-  })
-
-  connection.query(dayi_mysql_tables.sql_table_3,function(error,results,fields){
-    if(error){console.log(error);return;}
-    console.log('[dayi-info]Table 3 created successfully')
-  })
+// 插入文件记录
+function dayi_insert_file(file_path, user_name, file_uuid, file_size, file_name) {
+  const stmt = db.prepare(`INSERT INTO files (user_name, file_path, uuid, file_size, file_name) VALUES (?, ?, ?, ?, ?)`);
+  stmt.run(user_name, file_path, file_uuid, file_size, file_name);
 }
 
-function dayi_insert_file(file_path,user_name,file_uuid,file_size,file_name){
-  sql = 'INSERT INTO files SET ?'
-  sets = {'user_name':user_name,'file_path':file_path,'uuid':file_uuid,'file_size':file_size,'file_name':file_name};
-  connection.query(sql,sets,function(error,result,fields){
-    // console.log(result)
-    if(error){console.log(error);return;}
-  });
+// 插入用户记录
+function dayi_insert_user(user_name) {
+  const stmt = db.prepare(`INSERT INTO users (user_name, permission) VALUES (?, 1)`);
+  stmt.run(user_name);
 }
 
-function dayi_insert_user(user_name){
-  //(`id`, `user_name`, `passwd`, `permission`, `name`, `stu_number`, `json`) 
-  //VALUES (NULL, ?, NULL, \'1\', NULL, NULL, NULL);
-  sql = 'INSERT INTO `users` SET ?'
-  sets = {
-    'user_name':user_name,
-    'permission':1
-  }
-  connection.query(sql,sets,function(error,result,fields){
-    // console.log(result)
-    if(error){console.log(error);return;}
-  });
-}
-
-//查询用户是否存在于数据库中
+// 查询用户是否存在于数据库中
 async function dayi_query_user(user_name) {
-  return new Promise((resolve, reject) => {
-    sql = 'select * from `users` where ?'
-    sets ={
-      'user_name':user_name
-    }
-    connection.query(sql, sets, function(error, result, fields) {
-      if (error) {
-        console.log(error)
-        reject(error)
-      }
-      resolve(result.length >= 1)
-    })
-  })
+  const stmt = db.prepare(`SELECT * FROM users WHERE user_name = ?`);
+  const row = stmt.get(user_name);
+  return row != null;
 }
 
-
-async function dayi_user_files(user_name,file_uuid) {
-  return new Promise((resolve, reject) => {
-    sql = 'select * from `files` where ?'
-    sets ={
-      'user_name':user_name
-    }
-    if(file_uuid != undefined){
-      sql = "select * from `files` where user_name = ? and uuid = ?"
-      sets = [user_name,file_uuid]
-    }
-    connection.query(sql, sets, function(error, result, fields) {
-      if (error) {
-        console.log(error)
-        reject(error)
-      }
-      resolve(result)
-    })
-  })
+// 查询用户的文件记录，如果指定了 file_uuid，则只返回对应文件的记录
+async function dayi_user_files(user_name, file_uuid) {
+  let sql = `SELECT * FROM files WHERE user_name = ?`;
+  let values = [user_name];
+  if (file_uuid != undefined) {
+    sql += ` AND uuid = ?`;
+    values = [user_name, file_uuid];
+  }
+  const stmt = db.prepare(sql);
+  const rows = stmt.all(values);
+  return rows;
 }
 
-
-
+// 删除文件记录
 async function dayi_delete_file_sql(file_uuid) {
-  return new Promise((resolve, reject) => {
-    sql = 'delete from `files` where ?'
-    sets ={
-      // 'user_name':user_name,
-      'uuid':file_uuid
-    }
-    connection.query(sql, sets, function(error, result, fields) {
-      if (error) {
-        console.log(error)
-        reject(error)
-      }
-      resolve(result)
-    })
-  })
+  const stmt = db.prepare(`DELETE FROM files WHERE uuid = ?`);
+  stmt.run(file_uuid);
+}
+
+exports.dayi_query_user_permisson = (user_name) => {
+  const stmt = db.prepare(`SELECT permission FROM users WHERE user_name = ?`);
+  const row = stmt.get(user_name);
+  return row.permission;
 }
 
 
-async function dayi_query_user_permisson(user_name){
-  return new Promise((resolve, reject) => {
-    sql = 'select * from `files` where user_name = ?'
-    sets ={
-      'user_name':user_name,
-      // 'uuid':file_uuid
-    }
-    connection.query(sql, sets, function(error, result, fields) {
-      if (error) {
-        console.log(error)
-        reject(error)
-      }
-      resolve(result[0].permission)
-    })
-  })
-}
 
-
-dayi_create_tables()
-exports.dayi_query_user_permisson = dayi_query_user_permisson
 exports.dayi_delete_file_sql = dayi_delete_file_sql
 exports.dayi_user_files = dayi_user_files
 exports.dayi_insert_file = dayi_insert_file
