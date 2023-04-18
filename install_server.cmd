a8e4519439f91dd10905b64809a95401bbae70b3
dayi <yumibiao10@gmail.com>
2023-04-18 16:45:00 +0800

    install script

--- install_server.cmd
+++ install_server.cmd
@@ -1,3 +1,7 @@
 @REM 应该可以了
+
+
+
+
 cd server\file_manager\
-npm i -g yarn && yarn && cd .. && cd .. && cd server\print_really\ && pip install pdm && pdm install
+npm i -g yarn && yarn && cd .. && cd .. && cd server\print_really\ && pip install pdm && pdm install && echo Install proxy sevrer... && cd .. && cd .. && yarn 
