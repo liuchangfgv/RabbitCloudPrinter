589435ad37d171988b8f4e2ae324cea77ad19149
dayi <yumibiao10@gmail.com>
2023-04-18 23:26:53 +0800

    print-really-time

--- server/print_really/hi.py
+++ server/print_really/hi.py
@@ -10,6 +10,7 @@ from PIL import Image
 from docx2pdf import convert as doc2pdf
 # import fitz
 import requests
+import win32print
 
 app = Flask(__name__)
 
@@ -40,6 +41,38 @@ def api_v2_time():
     response_data = {
         "code": "201",
         "data": [current_time]
+    }
+    return jsonify(response_data)
+
+"""
+接口返回信息:
+{code:"201",data:["2021-03-03 19:23"]}
+#显示实时的打印信息
+"""
+@app.route(AUTH_key+"/api-v2/realtime-print-info", methods=['GET'])
+def api_v2_print_realtime():
+    def get_info():
+        res = ""
+        printer_name = win32print.GetDefaultPrinter()  # 获取默认打印机名称
+        h_printer = win32print.OpenPrinter(printer_name)  # 打开打印机句柄
+        # 获取打印机中所有作业
+        jobs = win32print.EnumJobs(h_printer, 0, -1, 1)
+        for job in jobs:
+            job_id, job_status = job["JobId"], job["Status"]
+            pages_printed, total_pages = job["PagesPrinted"], job["TotalPages"]
+            if total_pages > 0:
+                progress = int(pages_printed / total_pages * 100)
+                res+= f"作业ID：{job_id}，进度：{progress}%，状态：{job_status}"
+                
+            else:
+                res+= f"作业ID：{job_id}，进度：未知，状态：{job_status}"
+        win32print.ClosePrinter(h_printer)  # 关闭打印机句柄
+        return res
+
+    current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    response_data = {
+        "code": "201",
+        "data": get_info()
     }
     return jsonify(response_data)
 
