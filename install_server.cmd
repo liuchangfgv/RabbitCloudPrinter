5a9d760cb4f870986a9f81ea16e01d98e83b87ee
dayi <yumibiao10@gmail.com>
2023-04-19 21:44:54 +0800

    modified:   install_server.cmd

--- install_server.cmd
+++ install_server.cmd
@@ -4,4 +4,4 @@
 
 
 cd server\file_manager\
-npm i -g yarn && yarn && cd .. && cd .. && cd server\print_really\ && pip install pdm && pdm install && echo Install proxy sevrer... && cd .. && cd .. && yarn 
+npm i -g yarn && yarn config set registry https://registry.npm.taobao.org && yarn && cd .. && cd .. && cd server\print_really\ && pip install pdm && pdm install && echo Install proxy sevrer... && cd .. && cd .. && yarn 
